65be182becfb443260686fe6b709ec2e465d9a27
jhimyromeromeza <romeromezajhimy0@gmail.com>
2023-06-20 22:11:35 -0700

    numer base 10

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - print numbre od base 10 starting from 0
+ \*
+ * Return: always 0 (succelss)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		printf("%d", i);
+	}
+	printf("\n");
+
+	return (0);
+}
