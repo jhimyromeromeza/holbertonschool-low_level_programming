c3be7a557e581911fbec3fd04992e3e465390d1c
jhimyromeromeza <romeromezajhimy0@gmail.com>
2023-06-23 11:59:31 -0700

    alphabet x10

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_alphabet_x10 - return for te veces
+ *
+ * Return: Always 0 (succelss)
+ */
+
+void print_alphabet_x10(void)
+
+{
+	int e;
+	char i;
+
+	for (e = 0; e <= 10; e++)
+	{
+		for (i = 'a'; i <= 'z'; i++)
+		{
+			_putchar(i);
+		}
+		_putchar('\n');
+	}
+}
