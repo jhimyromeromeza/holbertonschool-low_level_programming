32a35229c3f03f535737cf93e0773ff48734d452
jhimyromeromeza <romeromezajhimy0@gmail.com>
2023-06-24 10:58:30 -0700

    abs

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _abs - print the value absolute
+ * @r: int check
+ * Return: Always 0
+ */
+
+int _abs(int r)
+
+{
+	int abNum = abs(r);
+		return (abNum);
+}
