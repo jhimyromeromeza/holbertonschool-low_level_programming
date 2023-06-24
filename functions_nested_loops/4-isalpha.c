975371894babc68bc037f8e805b026b0a560bd7b
jhimyromeromeza <romeromezajhimy0@gmail.com>
2023-06-23 17:37:30 -0700

    isalpha

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * _isalpha - look alphabet c and C.
+ * @c: variable check
+ *
+ * Return: Alwaya 0 and 1 (succelss)
+ */
+
+int _isalpha(int c)
+
+{
+	if ((c >= 'a' && c <= 'z') || ( c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
