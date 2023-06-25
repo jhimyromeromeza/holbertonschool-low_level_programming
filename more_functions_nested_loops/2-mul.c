222c3a61e7b0ca9a667ce8a953628b8f3c04f60f
jhimyromeromeza <romeromezajhimy0@gmail.com>
2023-06-25 09:01:10 -0700

    mul

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * mul - multiples two int
+ * @a: check number
+ * @b: check number two
+ *
+ * Return: Always resullt (succelss)
+ */
+
+int mul(int a, int b)
+
+{
+	printf("%d", a * b);
+	return (0);
+}
