df3df496d37360b1523b05123393e5ba1a0dfcc1
jhimyromeromeza <romeromezajhimy0@gmail.com>
2023-06-25 21:04:46 -0700

    numbers 98

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -12,19 +12,22 @@ void print_to_98(int n)
 {
 	int i;
 
-	if (n > 98)
+	if (n >= 98)
 	{
 		for (i = n; i >= 98; i--)
 		{
-			printf("%d, ", i);
+			if (i == 98)
+				printf("%d\n", i);
+			else
+				printf("%d, ", i);
 		}
 	}
-	if (n <= 98)
-	{
+	else
 		for (i = n; i <= 98; i++)
 		{
-			printf("%d, ", i);
+			if (i == 98)
+				printf("%d\n", i);
+			else
+				printf("%d, ", i);
 		}
-	}
-	printf("\n");
 }
