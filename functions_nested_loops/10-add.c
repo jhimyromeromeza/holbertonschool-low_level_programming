2dc5af1cf9aa7af86572a8fe619cdc6d9a82d590
jhimyromeromeza <romeromezajhimy0@gmail.com>
2023-06-25 19:47:18 -0700

    a+b

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * add - int result
+ * @a: number
+ * @b: number
+ *
+ * Return: Always result (succelss)
+ */
+
+int add(int a, int b)
+
+{
+	return (a + b);
+}
--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -4,7 +4,7 @@
  * times_table - print table
  *
  * Return: Always 0 (succelss)
- */
+ \*/
 
 void times_table(void)
 
@@ -13,17 +13,24 @@ void times_table(void)
 	int resultado;
 
 	for (i = 0; i <= 9; i++)
+	{
 		for (e = 0; e <= 9; e++)
 		{
 			resultado = i * e;
-			printf("%d", resultado);
-			if (e != 9)
+			if (e == 0)
+				printf("%d,  ", resultado);
+			if (resultado < 10)
 			{
-				printf(", ");
+				printf("  %d,", resultado);
 			}
-			else
+			else if (resultado >= 10)
+			{
+				printf(" %d,", resultado);
+			}
+			else if (e == 9)
 			{
 				printf("\n");
 			}
 		}
+	}
 }
