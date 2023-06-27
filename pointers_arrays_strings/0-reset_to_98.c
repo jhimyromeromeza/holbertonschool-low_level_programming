67824d2ce2da4dbf86ebc7a4803406445be2ecb9
jhimyromeromeza <romeromezajhimy0@gmail.com>
2023-06-27 09:27:42 -0700

    pointers

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * reset_to_98 - changer parameter of 98
+ * @*n: pointers parameter
+ *
+ * Return: Always changer of pointer (succelss)
+ */
+
+void reset_to_98(int *n)
+
+{
+	*n = 98;
+}
---
+++ pointers_arrays_strings/main.h
@@ -0,0 +1,5 @@
+#ifndef _main_h_
+#define _main_h_
+void reset_to_98(int *n);
+
+#endif
