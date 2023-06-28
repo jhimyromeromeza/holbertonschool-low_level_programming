d5147359ff6036ac0b7ab80846ee684953164ddd
jhimyromeromeza <romeromezajhimy0@gmail.com>
2023-06-27 19:33:01 -0700

    two of two

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * puts2 - immprimir caracteres
+ * @str: check pinters
+ *
+ * Return: numbers (succelss)
+ */
+
+void puts2(char *str)
+
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i = i + 2)
+	{
+		putchar(str[i]);
+	}
+	putchar('\n');
+}
