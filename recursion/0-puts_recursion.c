84ff15d7ddf241fb0fe3423a150b12e07cebd607
jhimyromeromeza <romeromezajhimy0@gmail.com>
2023-06-29 13:27:53 -0700

    print string with puts

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _puts_recursion - print a string
+ * @s: check pointers
+ *
+ * Return: Always string (succelss)
+ */
+
+void _puts_recursion(char *s)
+
+{
+	puts(s);
+}
