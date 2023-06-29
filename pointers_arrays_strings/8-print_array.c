e6a4003b63734fcf7a639e86275429a1d5be6301
jhimyromeromeza <romeromezajhimy0@gmail.com>
2023-06-29 10:02:09 -0700

    posicion of array

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_array - print n elements of array
+ * @a: check point array
+ * @n: numbers of elements
+ *
+ * Return: Always n array (succelss)
+ */
+
+void print_array(int *a, int n)
+
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+		{
+			if (i != n - 1)
+				printf(", ");
+			else
+				printf("\n");
+		}
+	}
+}
