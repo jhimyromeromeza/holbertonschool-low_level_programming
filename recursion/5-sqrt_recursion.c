5d36e13836a7e4d11c51785de01c6107cd1a28b0
jhimyromeromeza <romeromezajhimy0@gmail.com>
2023-06-29 19:14:59 -0700

    sqrt recursion

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,6 +1,8 @@
 #include "main.h"
 #include <stdio.h>
+
 int find_root(int n, int i);
+
 /**
  * _sqrt_recursion - main of number
  * @n: number check
@@ -14,10 +16,9 @@ int _sqrt_recursion(int n)
 	if (n < 0)
 	{
 		return (-1);
-	return (find_root(n, 0));
 	}
+	return (find_root(n, 0));
 }
-
 /**
  * find_root - finds the natural square root of a number
  * @n: number to calculate the square root of
@@ -27,6 +28,7 @@ int _sqrt_recursion(int n)
  */
 
 int find_root(int n, int i)
+
 {
 	if (i * i > n)
 	{
@@ -35,6 +37,6 @@ int find_root(int n, int i)
 	if (i * i == n)
 	{
 		return (i);
-	return (find_root(n, i + 1));
 	}
+	return (find_root(n, i + 1));
 }
