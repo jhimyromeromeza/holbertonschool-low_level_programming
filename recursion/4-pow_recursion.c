12111e0f6ae9302c26255f4d16b738445e73e882
jhimyromeromeza <romeromezajhimy0@gmail.com>
2023-06-29 17:22:58 -0700

    pow of number

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,30 @@
+#include <math.h>
+#include <stdio.h>
+#include "main.h"
+/**
+ * _pow_recursion - power of a number 
+ * @x: number check
+ * @y: number check
+ *
+ * Return: pow of number (succelss)
+ */
+
+int _pow_recursion(int x, int y)
+
+{
+	int potencia;
+
+	if (y < 0)
+	{
+		return (-1);
+	}
+	if (x == 0 || y == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		potencia = x * _pow_recursion(x, y - 1);
+		return (potencia);
+	}
+}
