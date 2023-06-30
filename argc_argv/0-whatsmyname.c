6ff386dcfab66649f1b842f0681fef04bb4226f5
jhimyromeromeza <romeromezajhimy0@gmail.com>
2023-06-30 11:53:38 -0700

    minameis

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - print is name
+ *
+ * Return: mi nama is (succels)
+ */
+
+int main(int argc, char *argv[])
+
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+		return (0);
+}
---
+++ argc_argv/README.md
@@ -0,0 +1,1 @@
+Learning with argc and argv.
