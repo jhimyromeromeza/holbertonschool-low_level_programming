c07bf50cae320b1bd5de59faeddd733e1436888b
jhimyromeromeza <romeromezajhimy0@gmail.com>
2023-07-02 17:00:40 -0700

    alloc grid

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+/**
+ * alloc_grid - return pointers
+ * @width: check pointers
+ * @height: check pointers
+ *
+ * Return: result (succelss)
+ */
+
+int **alloc_grid(int width, int height)
+
+{
+	int **grid;
+	int i, j;
+
+	if  (width <= 0 || height <= 0)
+		return (NULL);
+	grid = malloc(sizeof(int *) * height);
+	if (grid == NULL)
+		return (NULL);
+
+	for (i = 0; i < height; i++)
+	{
+		grid[i] = malloc(sizeof(int) * width);
+		if (grid[i] == NULL)
+		{
+			for (j = 0; j < i; j++)
+				free(grid[j]);
+			free(grid);
+			return (NULL);
+		}
+		for (j = 0; j < width; j++)
+			grid[i][j] = 0;
+	}
+	return (grid);
+}
