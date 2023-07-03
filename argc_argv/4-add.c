81bf841bcd71a87ad4ba9026f02ce9f8780e98f9
jhimyromeromeza <romeromezajhimy0@gmail.com>
2023-07-02 17:37:21 -0700

    add

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -12,20 +12,20 @@
 int main(int argc, char *argv[])
 
 {
-	 int i, j, sum = 0;
+	int i, j, sum = 0;
 
-	 for (i = 1; i < argc; i++)
-	 {
-		 for (j = 0; argv[i][j]; j++)
-		 {
-			 if (!isdigit(argv[i][j]))
-			 {
-				 printf("Error\n");
-				 return (1);
-			 }
-		 }
-		 sum += atoi(argv[i]);
-	 }
-	 printf("%d\n", sum);
-	 return (0);
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j]; j++)
+		{
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+	return (0);
 }
