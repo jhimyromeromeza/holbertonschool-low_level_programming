806abe9ef7a9b271339659af7f0faa9511520e15
jhimyromeromeza <romeromezajhimy0@gmail.com>
2023-07-05 06:30:52 -0700

    define a new struct

---
+++ structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - struct following elements
+ * @name: name of check pointers
+ * @age: check pointers
+ * @owner: check pointers 2
+ *
+ * Return: elements (succelss)
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
