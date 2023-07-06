d97095a55068a89c6f3a62dcc200795c19212e0a
jhimyromeromeza <romeromezajhimy0@gmail.com>
2023-07-06 07:30:58 -0700

    free memory

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * free_dog - frees dogs
+ * @d: check struct
+ *
+ * Return: result (succelss)
+ */
+
+void free_dog(dog_t *d)
+
+{
+	if (d == NULL)
+		return;
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
